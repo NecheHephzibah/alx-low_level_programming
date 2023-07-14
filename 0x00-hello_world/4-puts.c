ef93ffbafe8fe0b120eb9d266de342125d9efb4b
NecheHephzibah <neche.bless@gmail.com>
2023-07-14 06:29:14 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
+/**
+ * main - Entry point of the program
+ *
+ * return: always 0 (success)
+ */
 #include <stdio.h>
 
 int main(void)
 {
     puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+    return 0;
 }
