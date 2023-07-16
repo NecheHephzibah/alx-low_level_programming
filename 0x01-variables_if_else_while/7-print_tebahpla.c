687fb83cd0e9d25ab71a750f1725341a2bd28ac3
NecheHephzibah <neche.bless@gmail.com>
2023-07-16 13:56:04 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program,
+ * prints characters from 'z' to 'a' in reverse order.
+ *
+ * Return: Always 0 (Success).
+ */
+
+
+int main(void)
+{
+	char mib = 'z';
+	int tat = 26;
+
+	while (tat > 1)
+	{
+		putchar(mib);
+		mib--;
+		tat--;
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
+
