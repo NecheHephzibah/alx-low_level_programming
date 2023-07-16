bb95cea4078ae759d345874d8ee86acb082d1cea
NecheHephzibah <neche.bless@gmail.com>
2023-07-16 13:11:40 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point of the program,
+ * initializes input count to print out
+ * numbers from 1-9.
+ *
+ * Return: Always 0 (Success).
+ */
+
+int main(void)
+{
+
+	int count = 0;
+
+	while (count < 10)
+	{
+		printf("%d", count);
+		count++;
+	}
+	
+	printf("\n");
+
+	return (0);
+}
