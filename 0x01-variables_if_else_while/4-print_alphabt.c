8810c36253ec80788ff59e0a7e98a71ad6b18629
NecheHephzibah <neche.bless@gmail.com>
2023-07-16 12:41:20 -0700

    When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program,
+ * initializes inputs mib to print out 
+ * the alphabet in lowercase without q and e.
+ *
+ * Return: Always 0 (Success).
+ */
+
+
+int main(void)
+{
+
+	int tat = 0;
+	char mib = 'a';
+
+	while (tat < 26)
+	{
+		if (mib != 'q' && mib != 'e')
+		{
+			putchar(mib);
+		}
+		mib++;
+		tat++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
