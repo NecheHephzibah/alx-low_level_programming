0f1196db584ae110ab2b7c0cfc876276b25ba804
NecheHephzibah <neche.bless@gmail.com>
2023-07-19 10:56:39 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void); /*Function prototype for print_alphabeth*/
+
+int main(void)
+{
+    print_alphabet_x10();
+    return (0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+* print_alphabet - Entry point to the program, function print_alphabeth
+* prints alphabet a - z in lowercase.
+*
+* Description: program that prints out the alphabeth
+* in lowercase.
+*
+* Return: Always 0 (Success.
+*
+*/
+
+
+
+void print_alphabet_x10(void)
+{
+
+	char miber = 'a';
+	int hugo = '0';
+
+	while (hugo < 10)
+	{
+		char laurent = miber;
+
+		while (laurent <= 'z')
+		{
+			_putchar(laurent);
+			laurent++;
+		}
+		_putchar('\n');
+		miber++;
+		hugo++;
+	}
+}
+
+
