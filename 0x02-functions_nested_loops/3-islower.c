4b88fcc711dae40f0a0f7da09ebb0343dee576ee
NecheHephzibah <neche.bless@gmail.com>
2023-07-19 13:44:44 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -14,16 +14,5 @@
 int _islower(int c)
 {
 
-	if (c >= 'a' && c <= 'z')
-	{
-		_putchar('1');
-	}
-	else
-	{
-		_putchar('0');
-	}
-
-	_putchar('\n');
-
-	return (0);
-}
+	return (c >= 'a' && c <= 'z');
+}	
