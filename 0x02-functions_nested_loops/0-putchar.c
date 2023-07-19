5643ef97ec20d7559396461593c134ceb5304f91
NecheHephzibah <neche.bless@gmail.com>
2023-07-19 07:41:16 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,39 @@
+#include <unistd.h>
+#include "main.h"
+
+
+/** 
+* main - two main present, one main.h acting as
+* header file, second main function is the start of the
+* program.
+*
+* Description: created a program that outputs some
+* characters using the aid of my custom header.
+*
+* Return: Always 0 (Success).
+
+**/
+
+
+int _putchar(char c)
+{
+	return write(1, &c, 1);
+
+}
+
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Working on functions in C programming, calling them without the use of standard libraries.
