342d10935a752a6bf26dedee6f5d096c842b3f3d
NecheHephzibah <neche.bless@gmail.com>
2023-07-19 15:38:15 -0700

    Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table starting from 0.
+ */
+void times_table(void)
+{
+	int row, col, product;
+
+	for (row = 0; row <= 9; row++)
+		{
+		for (col = 0; col <= 9; col++)
+		{
+			product = row * col;
+
+            if (col == 0)
+			{
+				_putchar('0');
+			}
+			else
+			{
+				if (product <= 9)
+				_putchar(' ');
+				else
+				_putchar((product / 10) + '0');
+
+				_putchar((product % 10) + '0');
+			}
+
+			if (col != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
