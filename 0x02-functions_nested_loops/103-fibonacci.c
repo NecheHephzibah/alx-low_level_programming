77f911ceed23a7be1b258f10a7985dc228f655e1
NecheHephzibah <neche.bless@gmail.com>
2023-07-20 09:30:52 -0700

    Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+* main - finds and prints the sum of even
+* values.
+* Return: Always 0 (Success).
+*/
+
+
+int main(void)
+{
+	int hub;
+	unsigned long int jub, kub, next, sum;
+
+	jub = 1;
+	kub = 2;
+
+	for (hub = 1; hub <= 33; ++hub)
+	{
+		if (jub < 4000000 && (jub % 2) == 0)
+		{
+			sum = sum + jub;
+		}
+		next = jub + kub;
+		jub = kub;
+		kub = next;
+	}
+	printf("%lu\n", sum);
+
+	return (0);
+}
