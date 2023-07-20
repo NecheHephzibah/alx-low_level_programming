662a1e750e596a256c7d452c96d1b9b0c736f141
NecheHephzibah <neche.bless@gmail.com>
2023-07-20 10:05:52 -0700

    In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers. Instead, I wrote a program that would steal passwords of students. My teacher gave me an A+

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+* main - prints the sum of all the multiples of 3
+* or 5 below 1024 (excluded).
+* Return: Always 0 (Success).
+*/
+
+int main(void)
+{
+	int hub = 0;
+	unsigned long int jub = 0, kub = 1, next = 0;
+
+	while (hub < 98)
+	{
+		next = jub + kub;
+		jub = kub;
+		kub = next;
+		printf("%lu", next);
+
+		if (hub < 97)
+		{
+			printf(", ");
+			hub++;
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
