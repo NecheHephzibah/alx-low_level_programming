78d9e280e15ddc9e7a95c7e0410cccdc714a53c8
NecheHephzibah <neche.bless@gmail.com>
2023-07-19 17:24:22 -0700

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98.
+ * @n: The starting number.
+ */
+
+void print_to_98(int n)
+{
+
+	int increment = 1;
+
+	if (n > 98)
+	{
+
+		increment = -1;
+	}
+
+	while (n != 98)
+	{
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
+		_putchar(',');
+
+		if (n != 98)
+		{
+			_putchar(' ');
+		}
+
+		n += increment;
+	}
+
+	_putchar('9');
+	_putchar('8');
+	_putchar('\n');
+
+}
