153f183d12d4530a25c6aca76c05b22bee1ae074
NecheHephzibah <neche.bless@gmail.com>
2023-07-22 02:56:06 -0700

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+
+/**
+* main - function prints the numbers 1 - 100
+* with the multples of 3 and 5 outputting
+* Fizz and Buzz respectively and multiples of both 3 and 5
+* will output FizzBuzz.
+*
+* Return: 0
+*/
+
+
+int main(void)
+{
+	int count;
+
+	for (count = 1; count <= 100; count++)
+	{
+		if (count % 3 == 0)
+		{
+			printf("Fizz, ");
+		}
+		else if (count % 5 == 0)
+		{
+			printf("Buzz, ");
+		}
+		else if (count % 3 == 0 && count % 5 == 0)
+		{
+			printf("FizzBuzz, ");
+		}
+		else
+		{
+			printf("%d, ", count);
+		}
+	}
+	return (0);
+}
