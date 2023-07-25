3d56ec71332e8d958fefeb4187f4087c854a8584
NecheHephzibah <neche.bless@gmail.com>
2023-07-25 13:01:48 -0700

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -14,11 +14,11 @@ char *_strcpy(char *dest, char *src)
 	int y = 0;
 	int x = 0;
 
-	while (*(src + 1) != '\0')
+	while (*(src + y) != '\0')
 	{
 		y++;
 	}
-	for (; x < y; x++)
+	for ( ; x < y; x++)
 	{
 		dest[x] = src[x];
 	}
