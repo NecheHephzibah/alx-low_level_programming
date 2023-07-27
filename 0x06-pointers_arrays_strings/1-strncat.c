57febffb5e895f90006a7227ea03048b6246516f
NecheHephzibah <neche.bless@gmail.com>
2023-07-27 12:52:55 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* _strncat - function that concatenates two strings. 
+* @dest: destination buffer, where the concatenated string will be
+* stored..
+* @src: the source string that will be appended to the dest buffer.
+* @n: maximum number of characters to be copied from src
+* to dest.
+* Return: dest.
+*/
+
+
+char *_strncat(char *dest, char *src, int n)
+{
+	strncat(dest, src, n);
+	return (dest);	
+}
