cfaf009920efc325797d4f8357048d14b2f625d9
NecheHephzibah <neche.bless@gmail.com>
2023-07-28 02:50:26 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,6 +14,13 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	strncat(dest, src, n);
-	return (0);
+	int dest_length = strlen(dest);
+	int count;
+
+	for (count = 0; count < n && src[count] != '\0'; count++)
+	{
+		dest[dest_length + count] = src[count];
+		dest[dest_length] = '\0'; 
+	}
+	return (dest);
 }
