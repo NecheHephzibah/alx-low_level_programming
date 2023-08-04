eaae3ddfaafcf4ff1e18ee828ce43702d4f37ff8
NecheHephzibah <neche.bless@gmail.com>
2023-08-03 17:41:06 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _puts_recursion - prints a string to standard output.
+* @s: pointer to a string s.
+* Return: void.
+*/
+
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
