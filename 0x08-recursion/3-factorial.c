fe99d7263c052b3fb3b35dbbdd5962ac248f0722
NecheHephzibah <neche.bless@gmail.com>
2023-08-03 18:42:58 -0700

    You mustn't be afraid to dream a little bigger, darling

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,17 +8,9 @@
 
 int factorial(int n)
 {
-/*getting the base case first */
 	if (n < 0)
-    {
-        return (-1);
-    }
-	else if (n == 0)
-	{
-		return (0);
-	}
-	else
-	{
-		return (n * factorial(n - 1));
-	}
+		return (-1);
+	if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
 }
