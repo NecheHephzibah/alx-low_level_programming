f2d6764c86080e2b7ce7830ba81a128d2571a02b
NecheHephzibah <neche.bless@gmail.com>
2023-08-04 19:01:48 -0700

    Without libraries what have we? We have no past and no future

---
+++ 0x09-static_libraries/create_static_lib.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ar rc liball.a _abs.o _atoi.o _isalpha.o _isdigit.o _islower.o _isupper.o _memcpy.o _memset.o _memset.o _putchar.o 3-puts.o _strcat.o _strchr.o _strcmp.o _strcpy.o _strlen.o _strncat.o _strncpy.o _strpbrk.o _strspn.o _strstr.o
