c63c73975a3a2878d1c28d1fb95dbfd110272efd
NecheHephzibah <neche.bless@gmail.com>
2023-08-07 09:01:52 -0700

    Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+* main - entry point of my function
+* @argc: argument counter
+* @argv: argument vector which is an array pointer
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int product;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			product = atoi(argv[1]) * atoi(argv[2]);
+		}
+		printf("%d\n", product);
+	}
+	else if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
