621c79ead6ba818672517d861d5b2b63b59c8775
NecheHephzibah <neche.bless@gmail.com>
2023-08-08 11:00:39 -0700

    Minimal Number of Coins for Change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h> /* for INT_MAX */
+
+/**
+* main - Function thats the entry point of this programme
+* @argc: argument count passed into main function
+* @argv: argument vector which is an array of strings and can
+* also be converted to an interger using atoi function
+* Return: Always 0 (Success)
+*/
+
+int main(int argc, char *argv[])
+{
+	int cents;
+	int denomination[] = {25, 10, 5, 2, 1};
+	int numberCoins = sizeof(denomination) / sizeof(denomination[0]);
+	int minimumCoins = 0;
+	int i;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	cents = atoi(argv[1]);
+
+	if (cents < 0)
+	{
+		printf("0\n");
+	}
+
+	for (i = 0; i < numberCoins; i++)
+	{
+		while (cents >= denomination[i])
+		{
+			cents = cents - denomination[i];
+			minimumCoins++;
+		}
+	}
+	printf("%d\n", minimumCoins);
+
+	return (0);
+}
+
