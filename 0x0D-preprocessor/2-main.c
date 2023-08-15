9b074679159a31d3afb7747b74609acaf99ae23a
NecheHephzibah <neche.bless@gmail.com>
2023-08-15 12:49:38 -0700

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - function that outputs a predefined macro
+* @void: no parameter taken
+* Return: 0 success
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
