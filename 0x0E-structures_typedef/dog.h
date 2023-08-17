1fbe144ed7488c87b131c9206fa57b80d99d9c38
NecheHephzibah <neche.bless@gmail.com>
2023-08-17 03:53:29 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* struct dog - structure containing information on a dog
+* @name: name of the dog
+* @age: age of the dog
+* @owner: name of the owner of the dog
+* Return: 0
+*/
+
+struct dog{
+	char *name;
+	float age;
+	char *owner;
+};
