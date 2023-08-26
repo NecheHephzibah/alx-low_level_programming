852c434ff2e833ad02051c81df70870e01f2f65a
NecheHephzibah <neche.bless@gmail.com>
2023-08-25 19:43:27 -0700

    The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+void initializerHephzibah(void) __attribute__ ((constructor));
+
+/**
+* initializer - function that executes before the main function
+*/
+
+void initializer(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -35,6 +35,7 @@ list_t *add_node(list_t **head, const char *str);
 
 void free_list(list_t *head);
 
+void initializer (void);
 
 int nodeLength(const char *str);
 
