eb5d8b305f12d0a064a181696d5ea148a7c17a75
NecheHephzibah <neche.bless@gmail.com>
2023-08-25 17:07:37 -0700

    Add node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,58 @@
+#include "lists.h"
+
+int nodeLength(const char *str);
+
+/**
+* add_node - function that adds nodes to the beggining of a list
+* @head: This is the pointer to the another lists' head
+* @str: This is the string that'll be insertted to the beginning
+* list
+* Return: pointer to the first node in line, recently assigned
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newElement;
+
+	if (head == NULL)
+		return (NULL);
+
+	newElement = malloc(sizeof(list_t));
+
+	if (newElement == NULL)
+		return (NULL);
+
+	if (*head == NULL)
+		newElement->next = NULL;
+	else
+			newElement->next = *head;
+
+		newElement->str = strdup(str);
+		newElement->len = nodeLength(str);
+		*head = newElement;
+
+	return (*head);
+}
+
+
+/**
+* nodelength - calls or get the length of a string, str
+* @str: the string that it's length would be gotten
+* Return: length of str
+*/
+int nodeLength(const char *str)
+{
+	int mib;
+
+	if (str == NULL)
+		return (0);
+
+	mib = 0;
+
+	while (str[mib] != '\0')
+	{
+		mib++;
+	}
+
+	return (mib);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -23,5 +23,8 @@ size_t print_list(const list_t *h);
 
 size_t list_len(const list_t *h);
 
+list_t *add_node(list_t **head, const char *str);
+
+int nodeLength(const char *str);
 
 #endif /* _LISTS_H_ */
