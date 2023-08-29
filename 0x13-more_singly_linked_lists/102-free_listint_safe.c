2dc6469bf0ac395b6d9ef57b08788c06dfa8ba13
NecheHephzibah <neche.bless@gmail.com>
2023-08-29 15:05:44 -0700

    Free (safe version)

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - function that finds the loop in a linked list.
+ * @h: pointer to the first node
+ *
+ * Return: number of elements
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t len = 0;
+	int k;
+	listint_t *temp;
+
+	if (!h || !*h)
+		return (0);
+
+	while (*h)
+	{
+		k = *h - (*h)->next;
+		if (k > 0)
+		{
+			temp = (*h)->next;
+			free(*h);
+			*h = temp;
+			len++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			len++;
+			break;
+		}
+	}
+
+	*h = NULL;
+
+	return (len);
+}
