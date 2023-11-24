3fff286feed56d2101e5794d03b182d37f9998b7
NecheHephzibah <neche.bless@gmail.com>
2023-11-24 01:04:33 -0800

    created a function that coverts binary to decimal

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,39 @@
+
+#include "main.h"
+
+/**
+ * binary_to_unit - function that converts binary in string format to unsigned integers.
+ * @b: the user input which is in binary format goes here.
+ * Return: result, the converted binary, now an integer.
+ */ 
+
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+	unsigned int base = 1;
+	int length;
+	int i;
+
+	if (!b)
+		return (0);
+
+	for (length = 0; b[length] != '\0'; length++);
+
+	for (i = length - 1; i >= 0; i--)
+	{
+		if (b[i] != '0' && b[i] != '1')
+		{
+			return (0);
+		}
+
+		if (b[i] == '1')
+		{
+			result += base;
+		}
+
+		base = base * 2;
+	}
+
+	return (result);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,15 @@
+
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+
+
+#endif
+
