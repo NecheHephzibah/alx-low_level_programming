eb576ab94a9f315874ef9739cfa03dcbd3eedfe2
NecheHephzibah <neche.bless@gmail.com>
2023-11-28 02:55:43 -0800

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,8 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 		text_length = strlen(text_content);
 
-	my_file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	my_file_descriptor = open(filename, O_WRONLY | O_CREAT |
+		O_TRUNC, S_IRUSR | S_IWUSR);
 
 	if (my_file_descriptor == -1)
 		return (-1);
@@ -37,7 +38,7 @@ int create_file(const char *filename, char *text_content)
 			return (-1);
 
 	}
-	
+
 	close(my_file_descriptor);
-	return(1);
+	return (1);
 }
