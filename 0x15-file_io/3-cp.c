45e0666e71540679b41c9fb07f391a6969687907
NecheHephzibah <neche.bless@gmail.com>
2023-12-01 03:23:29 -0800

    Copies from one file to another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,81 +1,95 @@
-#define _POSIX_C_SOURCE 200809L
 #include "main.h"
 #include <fcntl.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
 
-int copy_file(char *source_path, char *destination_path);
+void check_argc(int argc, char *argv0);
+int open_files(char *source_path, char *destination_path,
+	int *source_fd, int *destination_fd);
+void copy_content(int source_fd, int destination_fd,
+	char *source_path, char *destination_path);
+void close_files(int source_fd, int destination_fd);
 
-/**
- * main - function that starts every C programming.
- * @ac: argument count, counts the total number of arguments
- * @av: argument vector, an array of all the arguments
- * Return: 0 (Always successful).
- */
-
-int main(int ac, char **av)
+int main(int argc, char **argv)
 {
-	ssize_t n;
+    int source_fd, destination_fd;
 
-	if (ac != 3)
-	{
-		dprintf(2, "Usage: %s cp file_from file_to\n", av[0]);
-		exit(1);
-	}
-	n = copy_file(av[1], av[2]);
-	printf("\n(printed chars: %li)\n", n);
+    check_argc(argc, argv[0]);
+    open_files(argv[1], argv[2], &source_fd, &destination_fd);
+    copy_content(source_fd, destination_fd, argv[1], argv[2]);
+    close_files(source_fd, destination_fd);
+
+    return (0);
+}
+
+void check_argc(int argc, char *argv0)
+{
+    if (argc != 3)
+    {
+        dprintf(2, "Usage: %s file_from file_to\n", argv0);
+        exit(97);
+    }
+}
+
+int open_files(char *source_path, char *destination_path,
+	int *source_fd, int *destination_fd)
+{
+    *source_fd = open(source_path, O_RDONLY);
+
+    if (*source_fd == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", source_path);
+        return (98);
+    }
+
+    *destination_fd = open(destination_path, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+
+    if (*destination_fd == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", destination_path);
+        close(*source_fd);
+        return (99);
+    }
 	return (0);
 }
 
-/**
- * copy_file - functions that copies file to another.
- * @source_path: pointer to the first file.
- * @destination_path: pointer to the second file.
- * Return: an integer.
- */
-
-int copy_file(char *source_path, char *destination_path)
+void copy_content(int source_fd, int destination_fd,
+	char *source_path, char *destination_path)
 {
-	int source_fd, destination_fd;
-	char buffer[1024];
-	ssize_t bytes_read;
+    char buffer[1024];
+    ssize_t bytes_read;
 
-	source_fd = open(source_path, O_RDONLY);
-	if (source_fd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", source_path);
-		exit(98);
-	}
+    while ((bytes_read = read(source_fd, buffer, sizeof(buffer))) > 0)
+    {
+        if (write(destination_fd, buffer, bytes_read) != bytes_read)
+        {
+            dprintf(STDERR_FILENO, "Error: Can't write to %s\n", destination_path);
+            close(source_fd);
+            close(destination_fd);
+            exit(99);
+        }
+    }
+    if (bytes_read == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", source_path);
+        exit(98);
+    }
+}
 
-	destination_fd = open(destination_path, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (destination_fd == -1)
+void close_files(int source_fd, int destination_fd)
+{
+	if (close(source_fd) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", destination_path);
-		close(source_fd);
-		exit(99);
-	}
-	while ((bytes_read = read(source_fd, buffer, sizeof(buffer))) > 0)
-	{
-		if (write(destination_fd, buffer, bytes_read) != bytes_read)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", destination_path);
-			close(source_fd);
-			close(destination_fd);
-			exit(99);
-		}
-	}
-	if (bytes_read == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", source_path);
-		exit(98);
-	}
-	if (close(source_fd) == -1 || close(destination_fd) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n",
-			(close(source_fd) == -1) ? source_fd : destination_fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", source_fd);
 		exit(100);
 	}
-	return (0);
+	if (close(destination_fd == -1))
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", destination_fd);
+		exit(100);
+	}
 }
+
