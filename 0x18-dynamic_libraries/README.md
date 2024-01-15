c572b6863e483a73cc8e0afee31c0df2ceaa8e77
NecheHephzibah <neche.bless@gmail.com>
2024-01-15 04:17:35 -0800

    Without libraries what have we? We have no past and no future | A library is not a luxury but one of the necessities of life.

---
+++ 0x18-dynamic_libraries/README.md
@@ -0,0 +1,1 @@
+This directry contains task on dynamic library, very different from static library. I'm to Create the dynamic library libdynamic.so containing all the functions listed in the question.
---
+++ 0x18-dynamic_libraries/strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _strcmp - function that compares two strings.
+* @s1: first buffer/string,
+*
+* @s2: the second string to used in comparison.
+*.
+* Return: si[i] - s2[i].
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+    int i;
+
+    for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+    {
+        if (s1[i] != s2[i])
+        {
+            return (s1[i] - s2[i]);
+        }
+    }
+    return (0);
+}
